6a75ae5aec5588eff7862bebbbd040fc16609e13
eduardsz <eduards.zvaigzne@gmail.com>
2020-12-18 19:51:10 +0200

    Added post method

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Body } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
 
   @Get()
-  getAll() {
+  getAll(): string {
     return 'getAll'
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id:string): string {
+    return 'getOne ' + id
+  }
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto): string {
+    return `Title: ${createProductDto.title}, price: ${createProductDto.price}`
+  }
+
+  @Put()
+  update() {
+
+  }
+
+  @Delete()
+  remove() {
+
   }
 }
