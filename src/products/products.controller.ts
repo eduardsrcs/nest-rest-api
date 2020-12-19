035ec17158592d971916d91a64c73bc6785de51e
eduardsz <eduards.zvaigzne@gmail.com>
2020-12-19 17:27:27 +0200

    made redirect

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post, Put, Body } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Body, Redirect } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
@@ -6,6 +6,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductsController {
 
   @Get()
+  // @Redirect('https://google.com', 301)
   getAll(): string {
     return 'getAll'
   }
@@ -21,8 +22,8 @@ export class ProductsController {
   }
 
   @Put(':id')
-  update(@Body() updateProductDto: UpdateProductDto) {
-
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return 'Update ' + id
   }
 
   @Delete(':id')
